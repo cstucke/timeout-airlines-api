e46c50197b739f7efd31c4ebb09c2c32232efcb3
Charles Stucke <charles-otto.stucke@epita.fr>
2025-12-13 20:25:20 +0100

    feat: Initial project commit

---
+++ backend/src/main/java/com/epita/timeout_airline/TimeoutAirlineApplication.java
@@ -0,0 +1,13 @@
+package com.epita.timeout_airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeoutAirlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeoutAirlineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/epita/timeout_airline/TimeoutAirlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epita.timeout_airline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeoutAirlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
