f6fe69a6490eb7a8636132bb79bf00efeb72f007
Armand <armand.vieau@epita.fr>
2025-12-14 16:22:38 +0100

    FlightRepository added + small fix on others

--- backend/src/main/java/com/epita/timeout_airline/repository/ClientRepository.java
+++ backend/src/main/java/com/epita/timeout_airline/repository/ClientRepository.java
@@ -2,7 +2,9 @@ package com.epita.timeout_airline.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.epita.timeout_airline.model.Client;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
