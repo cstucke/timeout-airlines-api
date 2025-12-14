c9f138ffbee223a6a3e468209e565d7e30aec627
Armand <armand.vieau@epita.fr>
2025-12-14 22:27:40 +0100

    All CRUD function implemented

--- backend/src/main/java/com/epita/timeout_airline/service/FlightService.java
+++ backend/src/main/java/com/epita/timeout_airline/service/FlightService.java
@@ -1,6 +1,7 @@
 package com.epita.timeout_airline.service;
 
 import com.epita.timeout_airline.repository.FlightRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.epita.timeout_airline.model.Flight;
@@ -9,10 +10,22 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 @Service
+@RequiredArgsConstructor
 public class FlightService {
 
-    @Autowired
     private FlightRepository flightRepository;
+
+    public Flight saveFlight(Flight flight) {
+        return flightRepository.save(flight);
+    }
+
+    public Optional<Flight> findById(Long id) {
+        return flightRepository.findById(id);
+    }
+
+    public List<Flight> findAll() {
+        return flightRepository.findAll();
+    }
 
     public List<Flight> searchFlights(String departureCity, String arrivalCity, LocalDate date) {
         LocalDateTime start = date.atStartOfDay();
@@ -20,5 +33,9 @@ public class FlightService {
         return flightRepository.findByDepartureCityAndArrivalCityAndDepartureTimeBetween(
                 departureCity, arrivalCity, start, end
         );
+    }
+
+    public void deleteFlight(Long id) {
+        flightRepository.deleteById(id);
     }
 }
