e72ef2580ee684ead6480ea9644c36e53bf5907f
Armand <armand.vieau@epita.fr>
2025-12-14 22:26:01 +0100

    Flight entity fixed for updates

--- backend/src/main/java/com/epita/timeout_airline/model/Flight.java
+++ backend/src/main/java/com/epita/timeout_airline/model/Flight.java
@@ -11,38 +11,28 @@ import java.time.LocalDateTime;
 public class Flight {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, unique = true)
-    private int flightNumber;
+    private String flightNumber;
 
     private String departureCity;
     private String arrivalCity;
 
+    private LocalDateTime departureTime;
+    private LocalDateTime arrivalTime;
+
     @ManyToOne
-    @JoinColumn(name = "departure_airport_id", nullable = false)
     private Airport departureAirport;
 
     @ManyToOne
-    @JoinColumn(name = "arrival_airport_id", nullable = false)
     private Airport arrivalAirport;
 
     @ManyToOne
-    @JoinColumn(name = "plane_id", nullable = false)
     private Plane plane;
 
-    @Column(nullable = false)
-    private LocalDateTime departureTime;
-    @Column(nullable = false)
-    private LocalDateTime arrivalTime;
+    private int numberOfSeat;
 
-    @Column(nullable = false)
-    private int numberOfSeats;
-
-    private Double firstClassSeatPrice;
-    private Double premiumSeatPrice;
-    private Double businessClassPrice;
-    private Double economyClassPrice;
+    private double firstClassSeatPrice;
+    private double premiumSeatPrice;
+    private double businessClassPrice;
+    private double economicsClassPrice;
 
 }
