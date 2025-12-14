a71d8c560d30fd3e2ee80ed75adb08a1c599e425
Armand <armand.vieau@epita.fr>
2025-12-14 17:02:23 +0100

    MilesReward entity added

---
+++ backend/src/main/java/com/epita/timeout_airline/model/MilesReward.java
@@ -0,0 +1,26 @@
+package com.epita.timeout_airline.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name="miles_rewards")
+public class MilesReward {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="client_id", nullable=false)
+    private Client client;
+
+    @ManyToOne
+    @JoinColumn(name="flight_id", nullable=false)
+    private Flight flight;
+
+    @Column(nullable = false)
+    private LocalDate date;
+}
